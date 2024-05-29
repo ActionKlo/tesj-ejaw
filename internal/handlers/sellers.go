5171aff67c3c4dcd2e7a82abaa572d01ca27f82e
Andrei <actionclaw@gmail.com>
2024-05-29 20:50:41 +0200

    use utils for responses

--- internal/handlers/sellers.go
+++ internal/handlers/sellers.go
@@ -1,21 +1,17 @@
 package handlers
 
 import (
-	"encoding/json"
 	"github.com/ActionKlo/test-ejaw/internal/data"
+	"github.com/ActionKlo/test-ejaw/internal/utils"
 	"net/http"
 )
 
 func GetSellers(w http.ResponseWriter, r *http.Request) {
 	sellers, err := data.Seller{}.GetAll()
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(err)
+		utils.SendError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(sellers)
+	utils.SendJSON(w, sellers)
 }
